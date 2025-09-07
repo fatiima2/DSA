49e673c7db67e7389e5c1e71c6ade1d3465ac0fe
fatiima2 <fatimazahid2915@gmail.com>
2025-09-07 13:55:27 +0500

    Q3 added

---
+++ LinkedList assignment 1/Q3.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+class Node {
+public:
+	int data;
+	Node* next;
+};
+class LinkedList {
+public:
+	Node* head = NULL;
+    void createNode(int data)
+	{
+		Node* node = new Node();
+		node->data = data;
+		node->next = head;
+		head = node;
+    }
+	void linkedListReverse()
+	{	
+		Node* temp = head;
+		Node* prev = NULL;
+		Node* Next = NULL;
+		while (temp!=NULL)
+		{
+			Next = temp->next;
+			temp->next = prev;
+			prev = temp;
+			temp = Next;
+			head = prev;
+		}
+		
+	}
+	void printLinkedList()
+	{
+		Node *temp = head;
+		while (temp != NULL)
+		{
+			cout << temp->data << " ";
+			temp = temp->next;
+		}
+		cout << endl;
+	}
+};
+int main()
+{
+	LinkedList list;
+	list.createNode(89);
+	list.createNode(20);
+	list.createNode(15);
+	list.createNode(50);
+	list.createNode(25);
+	list.createNode(80);
+	cout << "Original Linked List: \n";
+	list.printLinkedList();
+	cout << "Reversed Linked List: \n";
+	list.linkedListReverse();
+	list.printLinkedList();
+
+
+}
--- LinkedList assignment 1/Q4.cpp
+++ LinkedList assignment 1/Q4.cpp
@@ -1,39 +1,39 @@
-#include<iostream>
-using namespace std;
-class Node {
-public:
-	int data;
-	Node* next;
-};
-class LinkedList
-{
-public:
-	Node* head = NULL;
-	void insert(int data)
-	{
-		Node* node = new Node();
-		node->data = data;
-		node->next = head;
-		head = node;
-	}
-	void middleNode()
-	{
-		Node* temp = head;
-		Node* slowptr = head;
-		while (temp!=NULL && temp->next->next != NULL)
-		{
-			temp = temp->next->next;
-			slowptr = slowptr->next;
-		}
-		cout << "Middle-Node= " << slowptr->data;
-	}
-};
-int main()
-{
-	LinkedList list;
-	list.insert(27);
-	list.insert(20);
-	list.insert(89);
-	list.insert(10);
-	list.middleNode();
-}
+//#include<iostream>
+//using namespace std;
+//class Node {
+//public:
+//	int data;
+//	Node* next;
+//};
+//class LinkedList
+//{
+//public:
+//	Node* head = NULL;
+//	void insert(int data)
+//	{
+//		Node* node = new Node();
+//		node->data = data;
+//		node->next = head;
+//		head = node;
+//	}
+//	void middleNode()
+//	{
+//		Node* temp = head;
+//		Node* slowptr = head;
+//		while (temp!=NULL && temp->next->next != NULL)
+//		{
+//			temp = temp->next->next;
+//			slowptr = slowptr->next;
+//		}
+//		cout << "Middle-Node= " << slowptr->data;
+//	}
+//};
+//int main()
+//{
+//	LinkedList list;
+//	list.insert(27);
+//	list.insert(20);
+//	list.insert(89);
+//	list.insert(10);
+//	list.middleNode();
+//}
